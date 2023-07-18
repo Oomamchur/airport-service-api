ca6de6f173a41c9dc21d7563b06001ddd6fe08c4
Oleksandr Mamchur <irex1988@gmail.com>
2023-07-18 17:28:36 +0300

    add models

--- airport_service/models.py
+++ airport_service/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Airport(models.Model):
+    airport_name = models.CharField(max_length=60)
+    closest_big_city = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.airport_name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    distance = models.IntegerField()
+
+
+class AirplaneType(models.Model):
+    airplane_type = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.airplane_type
+
+
+class Airplane(models.Model):
+    airplane_name = models.CharField(max_length=60)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.airplane_name
